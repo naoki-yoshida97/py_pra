f1cd1e869b1eaf1192a6ebd87be32ebf1774d1ea
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2021-05-06 10:10:22 +0900

    5/6のぶん

--- hensu.py
+++ hensu.py
@@ -1,6 +1,30 @@
-s = input('整数a:')
-a = int(s)
-print(a)
-s = input('整数b:')
-b = int(s)
-print(b)
+# s = input('整数a:')
+# a = int(s)
+# print(a)
+# s = input('整数b:')
+# b = int(s)
+# print(b)
+
+# a = int(input('整数aを入力:'))
+# b = int(input('整数bを入力:'))
+# print('変数a:', a)
+# print('変数b:', b)
+
+c = float(input('実数aを入力:'))
+d = float(input('実数bを入力:'))
+print('実数a:', c)
+print('実数b:', d)
+
+r = float(input('球のの半径'))
+PI = 3.14159
+volume = 4/3 * PI * r**3
+print('球の体積は,', volume, '[m^3]です.')
+# print('a + b は', a+b, 'です。')  # 加算
+# print('a - b は', a-b, 'です。')  # 減算
+# print('a * b は', a*b, 'です。')  # 乗算
+# print('a / b は', a+b, 'です。')  # 除算
+# print('a // b は', a/b, 'です。')  # 切り捨て除算
+# print('a % b は', a % b, 'です。')  # 剰余(aをbで割ったあまり(剰余))
+# print('a ** b は', a**b, 'です。')  # べき乗
+# print('aとbの和は{}です。'.format(a+b))
+# print('{}と{}の和は{}です'.format(a, b, a+b))
