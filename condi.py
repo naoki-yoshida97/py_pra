e7af4c4ca1fc77d863ca2d6f8fc40b53c842681b
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2021-05-27 08:55:52 +0900

    add file

---
+++ condi.py
@@ -0,0 +1,21 @@
+n = int(input('整数値:'))
+
+if n > 0:
+    print('正の値です')
+elif n == 0:
+    print('0です')
+else:
+    print('負の値です')
+
+# a = int(input('整数a:'))
+# b = int(input('整数b:'))
+
+# if a == b:
+#      print('入力された値は等しいです')
+# else:
+#     print('入力された文字は等しくありません')
+
+# if a != b:
+#     print('入力された値は等しくありません')
+# else:
+#     print('入力された値は同値です')
---
+++ list41.py
@@ -0,0 +1,21 @@
+# n = int(input('整数n:'))
+#
+# if not (n<= -10 or n>=10):
+#     print('その値は二桁未満です')
+# else:
+#     print('その整数は二桁以上です')
+# mounth = input(int('季節を求めます/n月を入力してください'))
+
+a = int(input('整数a:'))
+b = int(input('整数b:'))
+if a - b <= 0 :
+    print('絶対値は',b-a,'です')
+else:
+    print('絶対値は',a-b,' desu')
+    # t = a
+    # a = b
+    # b = t
+
+# print('a≦bとなるようにソートしました')
+# print('変数aの値は', a, 'です')
+# print('変数bの値は', b, 'です')
