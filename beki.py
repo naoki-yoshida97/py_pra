5f5fca33b5ff278e6bb58ab3bc65fe30ff2aec6d
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2021-06-10 10:45:54 +0900

    add files

---
+++ beki.py
@@ -0,0 +1,4 @@
+n = int(input('整数:'))
+if n >= 0:
+    for n in range(0, n+1):
+        print(2**n)
---
+++ muge.py
@@ -0,0 +1,5 @@
+s = input('文字:')
+while s != "やめる":
+    s = input('文字:')
+    print(s)
+print('終わり')
